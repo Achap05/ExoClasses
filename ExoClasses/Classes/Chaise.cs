591d90b55270b195f7cad7e4a5bae45e4ac64426
Achap05 <chapon.art@gmail.com>
2024-07-09 14:36:59 +0200

    Ajoutez des fichiers projet.

---
+++ ExoClasses/Classes/Chaise.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExoClasses.Classes
+{
+    internal class Chaise
+    {
+        #region "Attributs"
+        private string _model;
+        private string _couleur;
+        private string _matière;
+        private int _nbrDePieds;
+        #endregion
+        
+        # region "Propriétés"
+        public string Model { get => _model; set => _model = value; }
+        public string Couleur { get => _couleur; set => _couleur = value; }
+        public string Matière { get => _matière; set => _matière = value; }
+        public int NbrDePieds { get => _nbrDePieds; set => _nbrDePieds = value; }
+        #endregion
+
+        #region "Constructeurs"
+        public Chaise()
+        {
+
+        }
+
+        public Chaise(string model, string couleur, string matière, int nbrDePieds)
+        {
+            Model = model;
+            Couleur = couleur;
+            Matière = matière;
+            NbrDePieds = nbrDePieds;
+        }
+        #endregion
+
+        #region Méthodes
+        public string Affiche()
+        {
+            return $"Je suis une Chaise en {_matière} {_couleur}, avec {_nbrDePieds} pieds";
+        }
+        #endregion
+    }
+
+}
---
+++ ExoClasses/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ExoClasses.Classes;
+
+
+/*
+Chaise
+Sujet
+Créer une classe Chaise possédant comme variables
+d’instance le nombre de pieds, le matériaux et la couleur
+de l’objet
+Afficher ses informations en surchargeant une méthode
+de la classe Object
+La classe Chaise pourra être instanciée avec ou sans
+paramètres (Constructeur par défaut)
+Afficher toutes les chaises (Possibilité de simplifier avec
+une méthode ToString)
+*/
+
+Chaise chaise1 = new Chaise();
+chaise1.NbrDePieds = 4;
+chaise1.Couleur = "blanche";
+chaise1.Matière = "bois";
+
+Chaise chaise2 = new Chaise("","noire","métal",3);
+
+Console.WriteLine(chaise1.Affiche());
+Console.WriteLine(chaise2.Affiche());
